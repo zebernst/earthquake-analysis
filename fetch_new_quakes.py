5fdd20872643075dc299dc23dd41dcca3e2f3068
Zach Bernstein <zebernst@gmail.com>
2016-11-08 21:59:23 -0500

    added printing to console if new events added to database

--- fetch_new_quakes.py
+++ fetch_new_quakes.py
@@ -11,18 +11,22 @@ DBSession = sessionmaker(bind=engine)
 session = DBSession()
 
 api_url = 'http://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/'
-feed_url = api_url + '{}_{}.geojson'.format('all', 'day')
+feed_url = api_url + '{}_{}.geojson'.format('all', 'month')
 # levels = ["significant", "4.5", "2.5", "1.0", "all"]
 # periods = ["hour", "day", "week", "month"]
+
+lastmodified = session.query(Quake).order_by(Quake.time.desc()).first()
+# TODO: automatically select correct feed to grab based on timedelta
 
 r = requests.get(feed_url)
 geodata = r.json()
 
 new_quakes = []
 for event in geodata['features']:
-    exists = session.query(exists().where(Quake.id == event['id'])).scalar()
-    if exists is False:
+    duplicate = session.query(exists().where(Quake.id == event['id'])).scalar()
+    if duplicate is not True:
         new_quakes.append(Quake._from_json(event))
 
 session.add_all(new_quakes)
 session.commit()
+print('Added {} new events to the database!'.format(len(new_quakes)))
