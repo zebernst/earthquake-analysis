5da53b3a19d431bcd3925a908e6147d39926d9c0
Zach Bernstein <zebernst@gmail.com>
2016-11-08 23:13:18 -0500

    added marker color corresponding to magnitude

--- plot_mag_worldmap.py
+++ plot_mag_worldmap.py
@@ -20,6 +20,18 @@ for event in session.query(Quake).all():
         lons.append(event.longitude)
         magnitudes.append(event.mag)
 
+
+def get_marker_color(magnitude):
+    # Returns green for small earthquakes, yellow for moderate
+    #  earthquakes, and red for significant earthquakes.
+    if magnitude < 3.0:
+        return ('go')
+    elif magnitude < 5.0:
+        return ('yo')
+    else:
+        return ('ro')
+
+
 eq_map = Basemap(projection='robin', resolution='l', area_thresh=1000.0,
                  lat_0=0, lon_0=-130)
 eq_map.drawcoastlines()
@@ -33,6 +45,7 @@ min_marker_size = 2.5
 for lon, lat, mag in zip(lons, lats, magnitudes):
     x, y = eq_map(lon, lat)
     msize = float(mag) * min_marker_size
-    eq_map.plot(x, y, 'ro', markersize=msize)
+    marker_string = get_marker_color(mag)
+    eq_map.plot(x, y, marker_string, markersize=msize)
 
 plt.show()
