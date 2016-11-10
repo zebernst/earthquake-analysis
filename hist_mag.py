d81f762cd7ca263a09893f5062bbbacb440ef16b
Zach Bernstein <zebernst@gmail.com>
2016-11-09 22:11:16 -0500

    added script to plot simple histogram of magnitudes to the terminal

---
+++ hist_mag.py
@@ -0,0 +1,26 @@
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy import create_engine
+from mpl_toolkits.basemap import Basemap
+import numpy as np
+import itertools
+from ascii_graph import Pyasciigraph
+
+from db_structure import Base, Quake
+
+engine = create_engine('sqlite:///quakes.db')
+Base.metadata.bind = engine
+
+DBSession = sessionmaker(bind=engine)
+session = DBSession()
+
+
+
+q = session.query(Quake.mag).filter(Quake.mag.isnot(None)).all()
+mags = list(itertools.chain.from_iterable(q))
+
+(vals, bins) = np.histogram(mags, bins='scott')
+histdata = list(itertools.zip_longest(map(str, bins), vals, fillvalue=0))
+
+graph = Pyasciigraph(line_length=120)
+for line in graph.graph('title', histdata):
+	print(line)
