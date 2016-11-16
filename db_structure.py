6bcc2b10c3e9b6b05d5f4190cd9f4688c90ad4b6
Zach Bernstein <zebernst@gmail.com>
2016-11-16 17:00:22 -0500

    Added tables for Feed objects, to save metadata (i.e. time generated)

--- db_structure.py
+++ db_structure.py
@@ -1,7 +1,9 @@
-from sqlalchemy import Column, create_engine
+from sqlalchemy import Table, Column, create_engine, ForeignKey
 from sqlalchemy import Integer, String, Float, DateTime, Interval, Boolean
+from sqlalchemy.orm import relationship
 from sqlalchemy.ext.declarative import declarative_base
-from datetime import datetime, timedelta
+from datetime import datetime, timedelta, timezone
+import requests
 
 
 def _int(value, default=int()):
@@ -40,15 +42,9 @@ def _bool(value, default=bool()):
         return default
 
 
-def _timestamp_ms(epoch):
-    if len(str(epoch)) == 13:
-        ms = int(str(epoch)[-3:])
-        epoch = float(str(epoch)[0:-3])
-
-    timestamp = datetime.fromtimestamp(float(epoch))
-    timestamp += timedelta(milliseconds=ms)
-
-    return timestamp
+def _timestamp_ms(value):
+    t = value / 1000.0
+    return datetime.fromtimestamp(t)
 
 
 Base = declarative_base()
@@ -58,10 +54,171 @@ class USGSException (Exception):
     pass
 
 
+url = 'http://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/{}_{}.geojson'
+ALLOWED_LEVELS = {"significant", "4.5", "2.5", "1.0", "all"}
+ALLOWED_PERIODS = {"hour", "day", "week", "month"}
+
+
+association_table = Table('association_table', Base.metadata,
+                          Column('feed_id', Integer, ForeignKey('feeds.id')),
+                          Column('quake_id', String, ForeignKey('quakes.id')))
+
+
+class Feed(Base):
+    __tablename__ = 'feeds'
+
+    id = Column(Integer, primary_key=True)
+
+    level = Column(String)
+    period = Column(String)
+
+    url = Column(String)
+    title = Column(String)
+    time = Column(DateTime)
+    api = Column(String)
+
+    bbox = relationship('BoundingBox', uselist=False, back_populates='feed')
+    quakes = relationship('Quake',
+                          secondary=association_table,
+                          back_populates='feeds')
+
+    def __init__(self, level, period):
+        """
+        Captures the feed with the given severity level and period.
+        ValueError is raised if either severity level or period are not
+        supported by the USGS data feeds.  IOError is raised if the
+        response status following the request for data is not 200 OK.
+        """
+        if level.lower() not in ALLOWED_LEVELS:
+            raise ValueError("invalid severity level")
+        if period.lower() not in ALLOWED_PERIODS:
+            raise ValueError("invalid period")
+
+        feed_url = url.format(level.lower(), period.lower())
+        response = requests.get(feed_url)
+
+        self.level = level.capitalize()
+        self.period = period.capitalize()
+
+        if response.status_code != 200:
+            message = "HTTP status code {:d}".format(response.status_code)
+            raise IOError(message)
+
+        self.data = response.json()
+        metadata = self.data['metadata']
+        self.url = metadata["url"]
+        self.title = metadata["title"]
+        self.time = datetime.fromtimestamp(metadata["generated"] / 1000.0,
+                                           tz=timezone.utc)
+        self.api = metadata["api"]
+        self.count = metadata['count']
+
+    # Special methods
+    def __len__(self):
+        """
+        Returns the number of events captured from the data feed.
+        """
+        return self.count
+
+    def __unicode__(self):
+        return "<Feed: {lvl} {per} | {dt}>".format(lvl=self.level,
+                                                   per=self.period,
+                                                   dt=self.time.isoformat())
+
+    def __repr__(self):
+        return "<Feed: {lvl} {per} | {dt}>".format(lvl=self.level,
+                                                   per=self.period,
+                                                   dt=self.time.isoformat())
+
+    def __str__(self):
+        return "<Feed: {lvl} {per} | {dt}>".format(lvl=self.level,
+                                                   per=self.period,
+                                                   dt=self.time.isoformat())
+
+    # Iterators for specific event data
+    @property
+    def events(self):
+        """
+        A generator that allows iteration over all events in the feed.
+        """
+        for event in self.data["features"]:
+            yield event
+
+    # Other methods
+    def refresh(self):
+        """
+        Updates this feed with new data.
+        IOError is raised if data acquisition fails.
+        """
+        response = requests.get(self.url)
+
+        if response.status_code != 200:
+            message = "HTTP status code {:d}".format(response.status_code)
+            raise IOError(message)
+
+        self.data = response.json()
+        metadata = self.data['metadata']
+        self.url = metadata["url"]
+        self.title = metadata["title"]
+        self.time = datetime.fromtimestamp(metadata["generated"] / 1000.0,
+                                           tz=timezone.utc)
+        self.api = metadata["api"]
+        self.count = metadata['count']
+
+
+class BoundingBox(Base):
+    __tablename__ = 'bboxes'
+
+    id = Column(Integer, primary_key=True)
+    min_longitude = Column(Float)
+    max_longitude = Column(Float)
+    min_latitude = Column(Float)
+    max_latitude = Column(Float)
+    min_depth = Column(Float)
+    max_depth = Column(Float)
+    feed_id = Column(Integer, ForeignKey('feeds.id'))
+
+    feed = relationship('Feed', back_populates='bbox')
+
+    def __init__(self, min_lon, min_lat, min_dep, max_lon, max_lat, max_dep):
+        self.min_longitude = min_lon
+        self.max_longitude = max_lon
+        self.min_latitude = min_lat
+        self.max_latitude = max_lat
+        self.min_depth = min_dep
+        self.max_depth = max_dep
+
+    # Special methods
+    def __unicode__(self):
+        return '<BoundingBox ({0:.2f}, {1:.2f}), ({2:.2f}, {3:.2f})>'.\
+            format(self.min_longitude, self.min_latitude,
+                   self.max_longitude, self.max_latitude)
+
+    def __repr__(self):
+        return '<BoundingBox ({0:.2f}, {1:.2f}), ({2:.2f}, {3:.2f})>'.\
+            format(self.min_longitude, self.min_latitude,
+                   self.max_longitude, self.max_latitude)
+
+    def __str__(self):
+        return '<BoundingBox ({0:.2f}, {1:.2f}), ({2:.2f}, {3:.2f})>'.\
+            format(self.min_longitude, self.min_latitude,
+                   self.max_longitude, self.max_latitude)
+
+    @staticmethod
+    def _from_json(json_data):
+        return BoundingBox(min_lon=json_data[0],
+                           min_lat=json_data[1],
+                           min_dep=json_data[2],
+                           max_lon=json_data[3],
+                           max_lat=json_data[4],
+                           max_dep=json_data[5])
+
+
 class Quake(Base):
     __tablename__ = 'quakes'
 
     id = Column(String, primary_key=True)
+    title = Column(String)
 
     mag = Column(Float)
     place = Column(String)
@@ -92,6 +249,10 @@ class Quake(Base):
     longitude = Column(Float)
     latitude = Column(Float)
     depth = Column(Float)
+
+    feeds = relationship('Feed',
+                         secondary=association_table,
+                         back_populates='quakes')
 
     def __init__(self, id, longitude, latitude, depth, mag, place, time,
                  updated, tz, url, detail, felt, cdi, mmi, alert, status,
@@ -198,5 +359,5 @@ class Quake(Base):
                      type=properties.get('type'))
 
 
-engine = create_engine('sqlite:///quakes.db')
+engine = create_engine('sqlite:///quakes_test.db')
 Base.metadata.create_all(engine)
