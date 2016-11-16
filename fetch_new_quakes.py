296f0167a1feb357578e128140ccfe19d1d1d1cb
Zach Bernstein <zebernst@gmail.com>
2016-11-16 17:00:53 -0500

    updated method for fetching and adding to database to incorporate Feed objects

--- fetch_new_quakes.py
+++ fetch_new_quakes.py
@@ -1,51 +1,65 @@
-from sqlalchemy.orm import sessionmaker
 from sqlalchemy import create_engine
-import requests
+from sqlalchemy.orm import sessionmaker
+from db_structure import Base, Feed, BoundingBox, Quake
 import inflect
-import pickle
-
-from db_structure import Base, Quake
-from db_structure import _timestamp_ms
+from tqdm import tqdm
+from matplotlib.cbook import dedent
 
 # Initialize database connection
-engine = create_engine('sqlite:///quakes.db')
+engine = create_engine('sqlite:///quakes_test.db')
 Base.metadata.bind = engine
 
 DBSession = sessionmaker(bind=engine)
 session = DBSession()
 
-# Fetch new earthquake data
-try:
-    with open('meta.pk', 'rb') as fi:
-        lastmeta = pickle.load(fi)
-except FileNotFoundError:
-    pass
 
-api_url = 'http://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/'
-feed_url = api_url + '{}_{}.geojson'.format('all', 'week')
-# levels = ["significant", "4.5", "2.5", "1.0", "all"]
-# periods = ["hour", "day", "week", "month"]
-geodata = requests.get(feed_url).json()
+# Fetch new earthquake data
+feed = Feed('all', 'day')
+feed.bbox = BoundingBox._from_json(feed.data['bbox'])
+session.add(feed)
+session.flush()
+
 
 # Add new events to database
-new_events = []
-saved_ids = set(list(zip(*session.query(Quake.id)))[0])
-for event in geodata['features']:
-    if event['id'] not in saved_ids:
-        new_events.append(Quake._from_json(event))
+def unique_instance(session, feed, event):
+    row = session.query(Quake).get(event['id'])
+    if not row:
+        row = Quake._from_json(event)
+        return row, True
+    if row:
+        new_inst = Quake._from_json(event)
+        for column in row.__table__.columns:
+            value = getattr(new_inst, column.name)
+            setattr(row, column.name, value)
+        return row, False
 
-session.add_all(new_events)
+tqdm_args = {
+    'desc': 'Updating database',
+    'total': feed.count,
+    'leave': False,
+    'unit': '',
+    'unit_scale': True,
+    'dynamic_ncols': True,
+    'bar_format': '{l_bar}{bar}| {n_fmt}/{total_fmt} '
+                  '[{elapsed}@ eta:{remaining}, {rate_fmt}]'
+    }
+
+new, upd = 0, 0
+for event in tqdm(iterable=feed.events, **tqdm_args):
+    instance, created = unique_instance(session, feed, event)
+    feed.quakes.append(instance)
+    if created:
+        new += 1
+    if not created:
+        upd += 1
+
+session.flush()
 session.commit()
 
-# Save feed metadata to disk
-metadata = geodata['metadata']
-metadata['generated'] = _timestamp_ms(metadata['generated'])
-
-with open('meta.pk', 'wb') as fi:
-    pickle.dump(metadata, fi)
-
 # Print status to console
-num = len(new_events)
-print('Added {num} new {events} to the database.'
-      .format(num=num,
-              events=inflect.engine().plural_noun('event', num)))
+p = inflect.engine()
+print(dedent('''
+             {new_events} added
+             {upd_events} updated
+             '''.format(new_events=p.no('event', new),
+                        upd_events=p.no('event', upd))))
