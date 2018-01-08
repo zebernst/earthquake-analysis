aad92cc70846c8542479081626587763a7b905a3
Zach Bernstein <zebernst@gmail.com>
2018-01-08 00:00:51 -0600

    Created Pipfile and adjusted imports accordingly

--- fetch_new_quakes.py
+++ fetch_new_quakes.py
@@ -2,7 +2,6 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from db_structure import Base, Feed, BoundingBox, Quake, valid_levels, valid_periods
 from tqdm import tqdm
-import inflect
 import argparse
 
 # Initialize database connection
